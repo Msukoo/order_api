0b33d757857662cd3f1a482bc8e35f8f0b7cbae6
Msukoo <rnalsttnn@gmail.com>
2021-05-24 22:43:11 +0900

    add service

--- src/main/java/com/alethio/service/controller/ServiceController.java
+++ src/main/java/com/alethio/service/controller/ServiceController.java
@@ -1,6 +1,7 @@
 package com.alethio.service.controller;
 
 import com.alethio.service.entity.Order;
+import com.alethio.service.service.OrderService;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,15 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 public class ServiceController {
-    Order order;
 
-    ServiceController(Order order){
-        this.order = order;
+    private OrderService orderService;
+
+    ServiceController(OrderService orderService){
+        this.orderService = orderService;
     }
+
     @RequestMapping(method = RequestMethod.POST, path = "/order")
     public String orderRequestApi(@RequestBody Order order){
         String result=null;
-
+        orderService.orderItem(order);
         return result;
     }
 }
---
+++ src/main/java/com/alethio/service/service/OrderService.java
@@ -0,0 +1,12 @@
+package com.alethio.service.service;
+
+import com.alethio.service.entity.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    public void orderItem(Order order){
+
+    }
+}
